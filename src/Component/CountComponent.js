8eb17d9b1534abd8dfc70337bc02292427e8edb8
rakshitha <rakshitha@robosoftin.com>
2022-03-12 20:54:19 +0530

    useSelector and useDispatch hook for utilizing state and dispatch

--- src/Component/CountComponent.js
+++ src/Component/CountComponent.js
@@ -1,10 +1,14 @@
 import React from 'react'
+import {useSelector,useDispatch} from "react-redux"
+import {incCount} from "../redux/count/countAction"
 
 export default function CountComponent() {
+    const count = useSelector(state => state.count);
+    const dispatch = useDispatch();
   return (
     <>
-        <h1>Count</h1>
-        <button>Add Count</button>
+        <h1>Count - {count}</h1>
+        <button onClick={() => dispatch(incCount())}>Add Count</button>
     </>
   )
 }
